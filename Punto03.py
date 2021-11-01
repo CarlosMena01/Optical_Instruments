4fb947402720f4a6fd2dc6bf689f634152cd3616
CarlosMenaPhysics <53710618+CarlosMena01@users.noreply.github.com>
2021-10-31 21:37:06 -0500

    Primera función para las lentes

--- Punto03.py
+++ Punto03.py
@@ -2,3 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+
+#Función para calcular la formación de imgenes de una lente en el plano focal si el objeto está en el otro plano focal
+def Lenses2F(image,landa,f):
+  result = (-1j/(landa*f))*np.fft.fftshift(np.fft.fftn(image))
+  return result

7a4b34b3fa1f7bbe26e7ca2cb02f52aa7a5e17b2
Emanuel Zapata Querubín <ezapataq@unal.edu.co>
2021-11-01 08:17:13 -0500

    Le quité el shift a la función Lenses2F (Es mejor utilizarlo a la hora de graficar, si es necesario)

--- Punto03.py
+++ Punto03.py
@@ -5,5 +5,5 @@ import cv2
 
 #Función para calcular la formación de imgenes de una lente en el plano focal si el objeto está en el otro plano focal
 def Lenses2F(image,landa,f):
-  result = (-1j/(landa*f))*np.fft.fftshift(np.fft.fftn(image))
+  result = (-1j/(landa*f))*(np.fft.fftn(image))
   return result

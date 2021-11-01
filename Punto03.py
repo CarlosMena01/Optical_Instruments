98ec92f347dccb3a39ee44f68003d917c18ccfb7
Emanuel Zapata Querubín <ezapataq@unal.edu.co>
2021-11-01 16:23:23 -0500

    Modifiqué la función Lenses2F para indicar si se hace o no el shift al calcular la transformada de Fourier

--- Punto03.py
+++ Punto03.py
@@ -4,6 +4,13 @@ import matplotlib.pyplot as plt
 import cv2
 
 #Función para calcular la formación de imgenes de una lente en el plano focal si el objeto está en el otro plano focal
-def Lenses2F(image,landa,f):
-  result = (-1j/(landa*f))*(np.fft.fftn(image))
+def Lenses2F(image,landa,f,shift):
+  #shift=1 Se hace el shift
+  #shift=0 No se hace el shift
+  #Como vimos, para la segunda parte del sistema el shift no es necesario
+  if (shift==1):
+    result = (-1j/(landa*f))*(np.fft.fftshift(np.fft.fftn(image)))
+  elif (shift==0):
+    result = (-1j/(landa*f))*(np.fft.fftn(image))
   return result
+

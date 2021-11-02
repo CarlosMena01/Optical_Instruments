5f19f6d1d5f52bf51314fe6775443669e2834fa6
CarlosMenaPhysics <53710618+CarlosMena01@users.noreply.github.com>
2021-11-02 16:57:51 -0500

    Se añadieron comentarios
    Para hacer más fácil la lectura de las funciones

--- Punto03.py
+++ Punto03.py
@@ -5,9 +5,10 @@ import cv2
 
 #Función para calcular la formación de imgenes de una lente en el plano focal si el objeto está en el otro plano focal
 def Lenses2F(image,landa,f,shift):
-  #shift=1 Se hace el shift
-  #shift=0 No se hace el shift
-  #Como vimos, para la segunda parte del sistema el shift no es necesario
+  #image: Imagen que se ubica en el foco del plano objeto y pasará por la lente 
+  #landa: Longitud de onda de la luente de luz que ingresa al sistema
+  #f: distancia focal de la lente
+  #shift: 0 si no se desea realizar un fftshit a la matriz y 1 en caso contrario
   if (shift==1):
     result = (-1j/(landa*f))*(np.fft.fftshift(np.fft.fftn(image)))
   elif (shift==0):
@@ -15,7 +16,9 @@ def Lenses2F(image,landa,f,shift):
   return result
 
 def Complex_Plot(matrix,kind,log):
+  #matrix: matriz a gráficar
   #kind: I=Intensidad, A=Amplitud, P=Fase
+  #log: 1 si se desea escala logarítmica 0 en caso contrario
   if (kind=='I'):
     matrix_to_plot=np.abs(matrix)**2
   elif (kind=='A'):

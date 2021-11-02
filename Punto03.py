e41a6fe503dd59e508e3777ef9e875965a85481a
CarlosMenaPhysics <53710618+CarlosMena01@users.noreply.github.com>
2021-11-02 17:51:56 -0500

    Gráfica sistema 4F
    Se agregaron comentarios
    Se corrigió errores con la escala logarítmica
    Se agregó la función para gráficar el sistema 4F

--- Punto03.py
+++ Punto03.py
@@ -15,7 +15,8 @@ def Lenses2F(image,landa,f,shift):
     result = (-1j/(landa*f))*(np.fft.fftn(image))
   return result
 
-def Complex_Plot(matrix,kind,log):
+#Función para gráficar matrices complejas 
+def Complex_Plot(matrix,kind,log,axs):
   #matrix: matriz a gráficar
   #kind: I=Intensidad, A=Amplitud, P=Fase
   #log: 1 si se desea escala logarítmica 0 en caso contrario
@@ -26,7 +27,36 @@ def Complex_Plot(matrix,kind,log):
   elif (kind=='P'):
      matrix_to_plot=np.angle(matrix)
   if (log==1):
-    plt.imshow(np.log(matrix_to_plot))
+    axs.imshow(np.log(matrix_to_plot + 0.0000001))
   elif (log==0):
-    plt.imshow(matrix_to_plot)
+    axs.imshow(matrix_to_plot)
   return 
+
+#Función para gráficar todo el sistema 4F
+def All_system(image,landa,f,kind):
+  #image: Imagen que se ubica en el foco del plano objeto y pasará por la lente 
+  #landa: Longitud de onda de la luente de luz que ingresa al sistema
+  #f: distancia focal de la lente
+  #kind: I=Intensidad, A=Amplitud, P=Fase
+  fig, axs = plt.subplots(1, 3, figsize=(9, 3), sharey=True)
+  Complex_Plot(image ,kind,0,axs[0])
+  axs[0].set_title("Imagen inicial")
+  Complex_Plot(Lenses2F(image,landa,f,1) ,kind,1,axs[1])
+  axs[1].set_title("Primera lente escala log")
+  Complex_Plot(Lenses2F(Lenses2F(image,landa,f,1),landa,f,0) ,kind,0,axs[2])
+  axs[2].set_title("Imagen final")
+  return
+
+#Definimos unidades
+um = 10**(-6)
+mm = 10**(-3)
+m = 1
+
+#Definimos los parametros y la imagen 
+root = ""
+filename = "red_heart.jpg"
+image = cv2.imread(root + filename, 0)
+landa = 0.66*um
+f = 2*m
+
+All_system(image, landa, f, "A")

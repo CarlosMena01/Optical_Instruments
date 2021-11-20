754787513d0a3dd5ac3e12aed2fc5fa9c9951e2f
Emanuel Zapata Querubín <ezapataq@unal.edu.co>
2021-11-20 11:20:27 -0500

    Ejemplo específico con espectro Angular

--- Entrega02/Punto01_DFT.py
+++ Entrega02/Punto01_DFT.py
@@ -65,3 +65,18 @@ def Angular_Spectrum_DFT(matrix,z,w_length,dx):
     U_z=DFT(A_z,'i')
     U_z=(df**2)*U_z
     return U_z
+
+""" A continuación se presenta un ejemplo del funcionamiento de la función de Espectro Angular via DFT
+    para la difracción de una apertura circular de diametro 3mm a una distancia de 250mm, iluminando
+    con luz de 650nm (ir al documento donde se compara el resultado con el resultado real) """
+
+#Se crea computacionalmente la apertura circular (el diámetro se acomoda con las dimensiones de muestreo)
+circ_aperture=np.zeros((500,500),dtype="uint8")
+coordy=int(np.shape(circ_aperture)[0]/2)
+coordx=int(np.shape(circ_aperture)[1]/2)
+cv2.circle(circ_aperture,(coordx,coordy),150,1,-1)
+
+#Se calcula el patrón de difracción por Espectro Angular
+U_circ=Angular_Spectrum_DFT(circ_aperture,250*mm,650*nm,1e-5)
+fig, axs = plt.subplots(1, 1, figsize=(10, 10))
+Complex_Plot(U_circ,'A',0,axs)

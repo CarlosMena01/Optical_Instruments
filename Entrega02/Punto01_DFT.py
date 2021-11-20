643896979df3772f5e03d4ae408fed4d8bda447c
Emanuel Zapata Querubín <ezapataq@unal.edu.co>
2021-11-20 11:39:59 -0500

    Implementación de la Transformada de Fresnel Discreta via DFTs

--- Entrega02/Punto01_DFT.py
+++ Entrega02/Punto01_DFT.py
@@ -80,3 +80,38 @@ cv2.circle(circ_aperture,(coordx,coordy),150,1,-1)
 U_circ=Angular_Spectrum_DFT(circ_aperture,250*mm,650*nm,1e-5)
 fig, axs = plt.subplots(1, 1, figsize=(10, 10))
 Complex_Plot(U_circ,'A',0,axs)
+
+""" Función de Difracción Discreta por Transformada de Fresnel usando DFTs"""
+def Fresnel_Transform_DFT(matrix,z,w_length,dx0):
+    # matrix es la representación discreta de la imagen o función 
+    # z es la distancia de propagación (metros)
+    # w_length es la longitud de onda (m)
+    # dx0 es el intervalo de muestreo de la función en el plano de entrada
+
+    #Se define la dimensión de la imagen (solo se utiliza N por ser cuadrada)
+    N=np.shape(matrix)[0]
+
+    #Se calcula dx, el intervalo de muestreo en el plano de salida
+    dx=(w_length*z)/(dx0*N)
+
+    #Se definen las coordenadas del plano de entrada con el fin de aplicar las fases esféricas de entrada
+    k=(2*np.pi)/w_length
+    x0=np.arange(-int(N/2),int(N/2),1)
+    y0=np.arange(-int(N/2),int(N/2),1)
+    n0,m0=np.meshgrid(x0,y0)
+
+    #Se aplican las fases esféricas de entrada
+    U_prima=matrix*np.exp((1j*k/(2*z))*((dx0*n0)**2+(dx0*m0)**2))
+
+    #Se calcula la transformada de Fourier de la función imediatamente anterior
+    U_2prima=DFT(U_prima)
+    U_2prima=(dx**2)*U_2prima
+
+    #Se definen las coordenadas del plano de salida con el fin de aplicar las fases esféricas de salida
+    x=np.arange(-int(N/2),int(N/2),1)
+    y=np.arange(-int(N/2),int(N/2),1)
+    n,m=np.meshgrid(x,y)
+
+    #Se aplican las fases esféricas de salida y se obtiene el campo difractado a un distancia z
+    U_z=U_2prima*(1)*np.exp((1j*k/(2*z))*((dx*n)**2+(dx*m)**2))
+    return U_z

a32f6656ccded33a78bbb2e8938ebbf4a7410b90
CarlosMena01 <cmena@unal.edu.co>
2021-11-24 21:37:53 -0500

    Punto 2a

--- Entrega02/Punto02a.py
+++ Entrega02/Punto02a.py
@@ -4,9 +4,9 @@ from resources.functions import *
 """ A continuación se presenta el analisis a los casos de difracción planteados """
 
 #Parámetros del sistema
-L = 0.2*mm
-m = 30
-N = 500 #Tamaño de la imagen
+L = 100*um
+m = 1
+N = 600 #Tamaño de la imagen
 dx = 10*um #Tamaño de pixel
 w_length = 650*nm #Longitud de onda
 z = (L**2)/w_length
@@ -23,10 +23,13 @@ Y = Y*dx
 image = (1/2)*(1 + m* np.cos(2*np.pi * X/L))
 
 #Gráficamos
-fig, axs = plt.subplots(1, 3, sharey=True)
+fig, axs = plt.subplots(1, 3)
 
-Complex_Plot(image,"A",0,axs[0])
-Complex_Plot(Diffraction(image,z,w_length,dx),"A",0,axs[1])
-Complex_Plot(Diffraction(image,z*1.3,w_length,dx),"A",0,axs[2])
-print("Z: ",z,"L: ",L, "WL: ", w_length )
+Complex_Plot(image,"A",0,axs[0], fig ,colbar= False)
+Complex_Plot(Diffraction(image,z*4,w_length,dx),"A",0,axs[1],fig, colbar= False)
+Complex_Plot(Diffraction(image,z*5,w_length,dx),"A",0,axs[2], fig,colbar= False)
+
+axs[0].set_title("Imagen original")
+axs[1].set_title("Difracción con N=4")
+axs[2].set_title("Difracción con N=5")
 plt.show()

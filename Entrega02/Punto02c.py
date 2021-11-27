f03d4320bb4118e9b2d7a22c1096dec062928271
CarlosMena01 <cmena@unal.edu.co>
2021-11-27 09:02:12 -0500

    Punto 2c

--- Entrega02/Punto02c.py
+++ Entrega02/Punto02c.py
@@ -2,12 +2,12 @@
 from resources.functions import *
 
 #Parámetros del sistema
-N = 600 #Tamaño de la imagen
+N = 300 #Tamaño de la imagen
 dx = 10*um #Tamaño de pixel
 w_length = 650*nm #Longitud de onda
-r = 150*dx
-z1 = (r**2)/(33*w_length)
-z2 = (r**2)/(32*w_length)
+r = 90*dx
+z1 = (r**2)/(21*w_length)
+z2 = (r**2)/(20*w_length)
 
 
 #Calculamos la transmitancia
@@ -20,7 +20,7 @@ Complex_Plot(image,"A",0,axs[0], fig ,colbar= False)
 Complex_Plot(Diffraction(image,z1,w_length,dx),"A",0,axs[1],fig, colbar= False)
 Complex_Plot(Diffraction(image,z2,w_length,dx),"A",0,axs[2],fig, colbar= False)
 axs[0].set_title("Rejilla circular")
-axs[1].set_title("Difracción con 33 zonas de Fresnel")
-axs[2].set_title("Difracción con 32 zonas de Fresnel")
+axs[1].set_title("Difracción con 21 zonas de Fresnel")
+axs[2].set_title("Difracción con 20 zonas de Fresnel")
 
 plt.show()

ac9bbf569c47e506147896b77de4d3b815ab6f4f
CarlosMena01 <cmena@unal.edu.co>
2021-11-27 23:04:35 -0500

    Nuevo 2a

--- Entrega02/Punto02a.py
+++ Entrega02/Punto02a.py
@@ -23,15 +23,26 @@ Y = Y*dx
 image = (1/2)*(1 + m* np.cos(2*np.pi * X/L))
 
 #Gráficamos
-fig, axs = plt.subplots(1, 3)
-
 #Tamaño del recorte
 N_cut = 40
-Complex_Plot(image[:N_cut,:N_cut],"A",0,axs[0], fig ,colbar= False)
-Complex_Plot(Diffraction(image,z*4,w_length,dx)[:N_cut,:N_cut],"A",0,axs[1],fig, colbar= False)
-Complex_Plot(Diffraction(image,z*5,w_length,dx)[:N_cut,:N_cut],"A",0,axs[2], fig,colbar= False)
 
-axs[0].set_title("Imagen original")
-axs[1].set_title("Difracción con N=4")
-axs[2].set_title("Difracción con N=5")
+fig, axs = plt.subplots()
+Complex_Plot(image[:N_cut,:N_cut],"A",0,axs, fig ,colbar= True)
+axs.set_title("Imagen original")
+axs.set_xlabel("0.01 mm/pixel")
+plt.show()
+
+fig, axs = plt.subplots(1, 3)
+
+
+Complex_Plot(Diffraction(image,z*4,w_length,dx)[:N_cut,:N_cut],"A",0,axs[0],fig, colbar= False)
+Complex_Plot(Diffraction(image,z*5,w_length,dx)[:N_cut,:N_cut],"A",0,axs[1], fig,colbar= False)
+Complex_Plot(Diffraction(image,z*4.5,w_length,dx)[:N_cut,:N_cut],"A",0,axs[2],fig, colbar= False)
+
+axs[0].set_title("Difracción con N=4")
+axs[1].set_title("Difracción con N=5")
+axs[2].set_title("Difracción con N=4.5")
+axs[0].set_xlabel("0.01 mm/pixel")
+axs[1].set_xlabel("0.01 mm/pixel")
+axs[2].set_xlabel("0.01 mm/pixel")
 plt.show()

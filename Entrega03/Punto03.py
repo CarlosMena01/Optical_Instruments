733b70a9d8cea5a4060fd150f6ef01f246ab0807
CarlosMena01 <cmena@unal.edu.co>
2021-12-18 07:59:45 -0500

    Complemento 3ro

--- Entrega03/Punto03.py
+++ Entrega03/Punto03.py
@@ -7,6 +7,7 @@ root = "resources/"
 file_name = "Hologram.tiff"
 Hologram = cv2.imread(root + file_name, 0)
 shape = np.shape(Hologram) #Dimensión del arreglo
+dx = (6513/shape[0])*um
 
 #Espectro de fourier
 Hologram_Fourier = np.fft.fftshift(np.fft.fftn(Hologram))
@@ -15,25 +16,49 @@ fig, axs = plt.subplots(1, 2, sharey=True)
 Complex_Plot(Hologram,'I',0,axs[0], fig)
 Complex_Plot(Hologram_Fourier,'I',1,axs[1], fig)
 axs[0].set_title("Holograma")
+axs[0].set_xlabel("{} um/pixel".format(round(dx/um,2)))
 axs[1].set_title("Espectro de frecuencias \n en escala logaritmica")
+axs[1].set_xlabel("{} um/pixel".format(round(dx/um,2)))
 plt.show()
 
-#Creamos un filtro para guardar la información de interés
+#Creamos los filtros para eliminar orden cero y orden -1
 r = 400 #Radio de los circulos 
 coor1=[1726,576] #Coordenadas donde se encuentra la información de interes 
-Filter = Mask_Circle(shape, r, "A", coor1[0],coor1[1])
-
+Filter1 = Mask_Circle(shape, r, "A", coor1[0],coor1[1]) #Filtro para eliminación de frecuencias
+coor2=[1725,580]
+Filter2 = Mask_Circle(shape, r, "A", coor2[0],coor2[1]) #Filtro para combinar métodos
 #Obtenemos la imagen filtrada
-obj = np.fft.ifftn(Hologram_Fourier*Filter)
+Hologram_Filter = np.fft.ifftn(Hologram_Fourier*Filter1)
 
-#Propagamos la imagen
+#Eliminamos el orden cero restando la media
+Hologram_Mean = Hologram - np.mean(Hologram)
+
+#Combinamos ambos métodos 
+Hologram_Mean_Fourier = np.fft.fftshift(np.fft.fftn(Hologram_Mean))
+Hologram_Mean_Filter = np.fft.ifftn(Hologram_Mean_Fourier*Filter1)
+
+#Propagamos las imagenes
 z = 73*mm
 landa = 633*nm
-dx = (6513/2048)*um
-obj_final = Diffraction(obj,z,landa,dx)
+dxf = landa*z/(shape[0]*dx) #Nuevo tamaño de pixel (Por ser difracción de fressnel)
+Hologram_Final = Diffraction(Hologram,z,landa,dx) #Reconstrucción sin eliminar orden cero
+Hologram_Filter_Final = Diffraction(Hologram_Filter,z,landa,dx) #Reconstrucción por filtrado de frecuencias
+Hologram_Mean_Final = Diffraction(Hologram_Mean,z,landa,dx) #Reconstruimos eliminando el orden cero por resta de media
+Hologram_Mean_Fourier_Final = Diffraction(Hologram_Mean_Filter,z,landa,dx) #Reconstruimos combinando metodos
+
 #Definimos las condiciones para la primer gráfica
-fig, axs = plt.subplots( sharey=True)
-Complex_Plot(obj_final, "A",0,axs)
-axs.set_title("Imagen reconstruida")
-axs.set_xlabel("{} um/pixel".format(round(dx/um,2)))
+fig, axs = plt.subplots(2,2, sharey=True)
+Complex_Plot(Hologram_Final, "A",0,axs[0][0])
+axs[0][0].set_title("Imagen reconstruida")
+axs[0][0].set_xlabel("{} um/pixel".format(round(dxf/um,2)))
+Complex_Plot(Hologram_Filter_Final, "A",0,axs[0][1])
+axs[0][1].set_title("Imagen reconstruida \n con filtrado de frecuencias")
+axs[0][1].set_xlabel("{} um/pixel".format(round(dxf/um,2)))
+Complex_Plot(Hologram_Filter_Final, "A",0,axs[1][0])
+axs[1][0].set_title("Imagen reconstruida \n con resta de media")
+axs[1][0].set_xlabel("{} um/pixel".format(round(dxf/um,2)))
+Complex_Plot(Hologram_Mean_Fourier_Final, "A",0,axs[1][1])
+axs[1][1].set_title("Imagen reconstruida \n combinando métodos")
+axs[1][1].set_xlabel("{} um/pixel".format(round(dxf/um,2)))
+plt.tight_layout()
 plt.show()
